acb6a983642fb75782c42e6085d769f408c90dd7
Andrea <andrea@gmail.com>
2020-02-20 12:49:53 -0800

    Cambios

--- Practica_01/practica_vue/src/config/auth.js
+++ Practica_01/practica_vue/src/config/auth.js
@@ -1,13 +1,31 @@
 import fireApp from './_firebase'
+//import router from '@/router'
 
 // import fireApp from './_firebase'
 
 export default {
+  login (data) {
+    fireApp.auth().signInWithEmailAndPassword(data.email, data.password).then(
+      (result) => {
+      }).catch((err) => {
+      console.log(err)
+    })
+  },
   signUp (data) {
     if (data.email === '' || data.password === '') {
       return alert('Todos los datos son obligatorios.')
     }
     fireApp.auth().createUserWithEmailAndPassword(data.email, data.password)
+      .then((result) => {
+        let newUser = {
+          displayName: data.nombre,
+          email: data.email,
+          uid: result.user.id
+        }
+        fireApp.auth().updateCurrentUser(newUser).catch(err => console.log(err))
+      }).catch((err) => {
+        console.table(err)
+      })
     console.log(data)
   }
 }
